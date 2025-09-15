360f136bfb7dfe6b996e5117da2b690755c4312d
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2025-09-15 11:47:27 -0700

    feat: data parsing

--- packages/app/src/components/sidebar_inputs/custom_data_input.tsx
+++ packages/app/src/components/sidebar_inputs/custom_data_input.tsx
@@ -2,7 +2,6 @@ import { Group, Text } from "@mantine/core";
 import { Dropzone } from "@mantine/dropzone";
 import { IconPhoto, IconUpload, IconX } from "@tabler/icons-react";
 import csv from "csvtojson";
-import { isNullish } from "../../utils";
 import { ContributionDay, ContributionWeeks } from "../../api/types";
 
 const toJSON = async (data: string, ext: string): Promise<Record<string, unknown>[]> => {
@@ -14,18 +13,18 @@ const toJSON = async (data: string, ext: string): Promise<Record<string, unknown
 }
 
 type ParseResult = {
-    error: string | undefined;
-    data: Record<string, unknown>[];
+    error: string | null;
+    data: ContributionWeeks[];
 }
 
-const isDate = (value: unknown) => /\d{4}-\d{2}\d{2}/.test(`${value}`);
+const isDate = (value: unknown) => !isNaN(new Date(`${value}`).getTime());
 const isNumber = (value: unknown) => /\d+/.test(`${value}`);
 
 const parseRecord = (record: Record<string, unknown>, dateKey: string, countKey: string): ContributionDay => {
     const date = new Date(record[dateKey] as string);
     const contributionCount = Number.parseInt(`${record[countKey]}`);
     const weekday = date.getUTCDay();
-    const color = "#40c463";
+    const color = "";
     return {
         date,
         contributionCount,
@@ -54,38 +53,51 @@ const parseRecords = (data: Record<string, unknown>[]): ParseResult => {
         return {
             error: "Unable to resolve fields for date and contribution count.",
             data: []
-        }
+        };
     }
     const valid = rest.every(entry => isDate(entry[dateKey]) && isNumber(entry[dateKey]));
     if (!valid) {
         return {
             error: `Found a record that does not conform to inferred schema (dateKey: ${dateKey}, countKey: ${countKey})`,
             data: []
+        };
+    }
+    const records = data
+        .map(d => parseRecord(d, dateKey, countKey))
+        .sort((r1, r2) => r1.date.getTime() - r2.date.getTime());
+    const START_YEAR = records[0].date.getFullYear();
+    const years: ContributionWeeks[] = records.reduce<ContributionWeeks[]>((acc, curr) => {
+        const idx = curr.date.getFullYear() - START_YEAR;
+        acc[idx] ??= [];
+        if (curr.weekday === 0 || acc[idx].at(-1) === undefined) {
+            acc[idx].push({
+                contributionDays: [],
+                firstDay: curr.date
+            });
         }
-    }
-    const weeks: ContributionWeeks[] = [[]];
-    const parsedBaseline = parseRecord(baseline, dateKey, countKey);
-    let prevWeekday = parsedBaseline.weekday;
-    let yearIdx = 0;
-    for (const record of data) {
-        const parsed = parseRecord(record, dateKey, countKey);
-    }
+        acc[idx].at(-1)!.contributionDays.push(curr);
+        return acc;
+    }, []);
+
+    return {
+        error: null,
+        data: years
+    };
 }
 
 export function CustomDataInput() {
-
-
     return (
         <Dropzone
             onDrop={async ([file]) => {
                 const [name, ext] = file.name.split(".");
                 const data = await file.text();
                 const json = await toJSON(data, ext);
+                console.log(name, json);
+                console.log(parseRecords(json));
             }}
             onReject={(files) => console.log('rejected files', files)}
             accept={["application/json", "text/csv"]}
             maxFiles={1}
-
         >
             <Group justify="center" gap="sm" mih={140} style={{ pointerEvents: 'none' }}>
                 <Dropzone.Accept>
